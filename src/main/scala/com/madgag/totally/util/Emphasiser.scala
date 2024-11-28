c3700a787b7795a10a7278df8a551dff6a153495
Roberto Tyley <52038+rtyley@users.noreply.github.com>
2024-11-28 14:55:29 +0000

    Update Emphasiser.scala

--- src/main/scala/com/madgag/totally/util/Emphasiser.scala
+++ src/main/scala/com/madgag/totally/util/Emphasiser.scala
@@ -5,6 +5,8 @@ trait Emphasiser {
 }
 
 object Emphasiser {
+  println("I could be some REALLY MALICIOUS CODE - but I'm just a demonstration! Sorry about that!")
+  
   val Sardonic = new Emphasiser {
     override def emphasise(s: String): String = s"_${s}_"
   }
